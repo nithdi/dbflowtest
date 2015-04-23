5add4f354432d619ef7f916a02c1b6f0ddbd5818
unknown <GBaumgarten@W7-GBAUMGARTEN.avm.de>
2015-04-23 14:09:53 +0200

    sample test

---
+++ app/src/androidTest/java/avm/de/dbflowtet/DatabaseTest.java
@@ -0,0 +1,11 @@
+package avm.de.dbflowtet;
+
+import android.test.InstrumentationTestCase;
+
+public class DatabaseTest extends InstrumentationTestCase {
+
+   public void test1() {
+       assertEquals(true, true);
+   }
+
+}
