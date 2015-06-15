3254fd209d2de6c5cead8973d6a2675d05239a94
unknown <GBaumgarten@W7-GBAUMGARTEN.avm.de>
2015-06-15 16:36:09 +0200

    Noch ein Test

---
+++ app/src/test/java/avm/de/dbflowtet/ExtensiveTest.java
@@ -0,0 +1,35 @@
+package avm.de.dbflowtet;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21)
+public class ExtensiveTest {
+
+    @Test
+    public void testSomething() throws Exception {
+        Assert.assertFalse(false);
+    }
+
+    @Test
+    public void testElse() throws Exception {
+        Assert.assertFalse(false);
+    }
+
+    @Test
+    public void testSomethingElse() throws Exception {
+        Assert.assertFalse(false);
+    }
+
+    @Test
+    public void testMe() throws Exception {
+        Assert.assertFalse(false);
+    }
+
+}
